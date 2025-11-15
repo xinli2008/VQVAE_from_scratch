a6793eae31bd9017dccfb13a73cc864298bab23d
xinli2008 <czu_lixin@163.com>
2025-11-15 17:55:28 +0800

    Update loss comment

--- main.py
+++ main.py
@@ -19,9 +19,22 @@ def get_dataloader(dataset_type, batch_size, img_shape, is_train = True):
     else:
         raise ValueError(f"unsupported type for {dataset_type}")
 
-    # 创建dataloader
-    # dataloader的num_workers参数决定了数据加载时使用的工作进程的数量
-    # 也就是在训练过程中, 多少个并行的进程同时负责从磁盘来加载数据
+    r"""
+    创建Dataloader对象
+    参数:
+        dataset: 数据集对象
+        batch_size: 每个batch的大小
+        shuffle: 是否在每个epoch开始时打乱数据
+        num_workers: 用于数据加载的子进程数量(0表示主进程加载)
+        collate_fn: 用于将样本列表合并为一个batch的函数
+        sampler: 自定义采样器, 与shuffle互斥
+        batch_sampler: 自定义batch采样器, 与batch_size, shuffle, sampler互斥
+        pin_memory: 是否将数据加载到GPU的固定内存中, 以加快数据传输速度
+        drop_last: 如果数据集大小不能被batch_size整除, 是否丢弃最后一个不完整的batch
+        timeout: 数据加载的超时时间(秒)
+    returns:
+        dataloader对象  
+    """
     dataloader = DataLoader(dataset, batch_size, shuffle = True, num_workers = 4)
     return dataloader
 
@@ -58,17 +71,24 @@ def train_vqvae(
             current_batch_size = x.shape[0]
             x = x.to(device)
 
-            # 前向传播
+            # NOTE：VQVAE的前向传播会返回三个值: 重构的x_hat, 编码器输出ze, 量化后的zq
             x_hat, ze, zq = model(x)
 
-            # NOTE:损失计算
-            # VQVAE的loss主要分为三个部分: 重建loss、嵌入loss和承诺loss
-            # 重建loss: 原始图像x和重建图像x_hat之间的差异。
-            # 嵌入loss：量化后的zq和ze之间的差异。
-            # 承诺loss: 使ze接近zq
+            # NOTE: VQVAE的损失函数由三部分组成: 重构损失, 嵌入损失, 和承诺损失
+            # 1. 重构损失: 衡量的是输入x和重构x_hat之间的差异, 通常使用均方误差(MSE)来计算
+            # 2. 嵌入损失： 衡量的是编码器输出ze和量化后的zq之间的差异, 通过最小化这个损失, 可以确保编码器输出接近于量化后的表示
+            # 3. 承诺损失: 衡量的是编码器输出ze和量化后的zq之间的差异, 通过最小化这个损失, 可以确保量化后的表示不会偏离编码器输出太远
+
+            # NOTE：可以看到后面两个loss虽然计算的是相同的MSE值，但是梯度流向完全不同。
             loss_reconstruct = mse_loss(x, x_hat)
+            # 1. loss_embedding = mse_loss(ze.detach(), zq)：梯度只流向了zq，因为ze被detach了，所以它是更新码本，使得码本向量zq靠近编码器输出ze。
+            # 2.  loss_commitment = mse_loss(ze, zq.detach())：梯度只流向了ze，因为zq被detach了，所以它是更新编码器，使得编码器输出ze向量向码本向量zq靠近。
+            # 这两个损失项共同作用，使得编码器和码本相互靠近，从而实现有效的向量量化。
             loss_embedding = mse_loss(ze.detach(), zq)
             loss_commitment = mse_loss(ze, zq.detach())
+            # NOTE: 如果只有1个方向的loss的话，比如说只有loss_embedding的话, 则编码器可以随意输出，码本疲于追赶，训练不稳定。
+            # 同理，如果只有loss_commitment的话，码本可以随意输出，编码器疲于追赶，也会导致训练不稳定。
+
             loss = loss_reconstruct + loss_embedding_weight * loss_embedding +  loss_commitment_weight * loss_commitment
 
             # 反向传播和优化
